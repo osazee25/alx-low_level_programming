901d2fbe55852df170c0723f0984063a0637e94f
osazee25 <osazeemarvins@yahoo.com>
2023-02-16 11:54:39 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a text
+ * Return: 1
+ */
+main(void)
+{
+	printf("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n");
+	return (1);
+}
