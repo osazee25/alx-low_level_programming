8712bc38b53c7b2453c8bf2de5e0338e570ec958
osazee25 <osazeemarvins@yahoo.com>
2023-02-16 12:19:03 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 main(void)
 {
-	printf("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n");
+	write(2, "and that piece of art is useful -Dora Kopar, 2015-10-19\n", 59);
 	return (1);
 }
