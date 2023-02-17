4b4e5ce0dae4a8e5ee914618527a57a5ecc2d89c
osazee25 <osazeemarvins@yahoo.com>
2023-02-17 14:17:59 -0800

    num10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all single digits of numbers of base 10
+ * sterting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+	printf("%d\n", n);
+	n++;
+	}
+	return (0);
+}
