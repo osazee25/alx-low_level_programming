1ce0c7f34c0a96a84a7840cf82c75b0bf7e21acc
osazee25 <osazeemarvins@yahoo.com>
2023-02-17 16:13:57 -0800

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all the numbers of base 16 in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	char ch;
+
+	for (n = 0; n <= 9; n++)
+	putchar(n);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
