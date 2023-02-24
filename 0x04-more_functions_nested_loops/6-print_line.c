b033d4c3c81bfd47f8faaba3d90bc35d383a535e
osazee25 <osazeemarvins@yahoo.com>
2023-02-24 01:31:10 -0800

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,17 +4,20 @@
  * @n: the number of times the character _ is drawn
  * Return: a straight line
  */
-void print_line(int n);
+void print_line(int n)
 {
 	int n;
 
-	for (n = 0; n <= n; n++)
-	{
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
-	_putchar(95);
+	else
+	{
+	for (n = 0; n <= n; n++)
+	{
+	putchar(95);
 	}
-	putchar('\n);
+	putchar('\n');
+	}
 }
