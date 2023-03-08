93a341ea97c2ca7ce92fd98352ccf1136dc91624
osazee25 <osazeemarvins@yahoo.com>
2023-03-08 12:43:54 -0800

    reverse recur

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -print_rev_recursion - print s string in reverse
+ * _print_rev_recursion - print s string in reverse
  *  @s: the string to reverse
  *  Return: Nothing
  */
