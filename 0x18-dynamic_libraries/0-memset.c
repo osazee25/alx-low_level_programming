87eb70b0d3ad2870d04d846f6a8baef45218a8de
osazee25 <osazeemarvins@yahoo.com>
2023-09-18 18:07:46 -0700

    dynamic lib

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		s[c] = b;
+	}
+
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - a function that returns a string
+ * @s: the string to be returned
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
